9b64f13515c135b026d5d4f8323f024db13157ea
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-08-17 18:10:55 +0200

    simplify starter

--- playground-fe/src/main.ts
+++ playground-fe/src/main.ts
@@ -1,14 +1,36 @@
-import './assets/main.css'
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
+import setupGame from './game/game-starter.ts';
+import { PixiAppWrapper } from './game/pixi/pixi-app-wrapper.ts';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const querySelector = <E extends Element = Element>(selector: string): E => {
+  const element = document.querySelector<E>(selector);
+  if (!element) {
+    throw Error(`${selector} is null`);
+  }
+  return element;
+};
 
-import App from './App.vue'
-import router from './router'
+querySelector<HTMLDivElement>('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+setupCounter(querySelector<HTMLButtonElement>('#counter'));
+const wrapper = new PixiAppWrapper({});
+setupGame(querySelector<HTMLDivElement>('#app'), wrapper);
