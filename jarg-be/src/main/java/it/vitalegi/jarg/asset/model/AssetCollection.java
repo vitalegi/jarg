c9421fbdced36bc2d0a360bc2baa4912cc83407a
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-08-28 23:58:25 +0200

    cleanup

--- jarg-be/src/main/java/it/vitalegi/jarg/asset/model/AssetCollection.java
+++ jarg-be/src/main/java/it/vitalegi/jarg/asset/model/AssetCollection.java
@@ -1,11 +1,15 @@
 package it.vitalegi.jarg.asset.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
 
+@Schema(description = "A collection contains a list of animations")
 @Data
 public class AssetCollection {
+    @Schema(description = "Collection's URL")
     String url;
+    @Schema(description = "Available animations in this collection")
     List<String> animations;
 }
--- jarg-be/src/main/java/it/vitalegi/jarg/resource/SkinResource.java
+++ jarg-be/src/main/java/it/vitalegi/jarg/resource/SkinResource.java
@@ -1,14 +1,17 @@
 package it.vitalegi.jarg.resource;
 
+import io.swagger.v3.oas.annotations.Operation;
+import it.vitalegi.jarg.asset.model.AssetCollection;
 import it.vitalegi.jarg.asset.service.AssetService;
 import it.vitalegi.jarg.logging.Performance;
 import it.vitalegi.jarg.logging.Type;
-import it.vitalegi.jarg.asset.model.AssetCollections;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Log4j2
 @RequestMapping("/skins")
@@ -19,10 +22,9 @@ public class SkinResource {
     @Autowired
     AssetService service;
 
+    @Operation(description = "Retrieve all collections containing skins")
     @GetMapping("/collections")
-    public AssetCollections getSkinsCollections() {
-        var out = new AssetCollections();
-        out.setCollections(service.getSkinsCollections());
-        return out;
+    public List<AssetCollection> getSkinsCollections() {
+        return service.getSkinsCollections();
     }
 }
