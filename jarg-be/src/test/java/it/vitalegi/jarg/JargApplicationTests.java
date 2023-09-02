cd9f4a3f0c319cfbb1d9351d9109d4e81a6e2b1c
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-09-02 19:55:07 +0200

    fix tests

--- jarg-be/src/test/java/it/vitalegi/jarg/JargApplicationTests.java
+++ jarg-be/src/test/java/it/vitalegi/jarg/JargApplicationTests.java
@@ -2,8 +2,10 @@ package it.vitalegi.jarg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JargApplicationTests {
 
 	@Test
