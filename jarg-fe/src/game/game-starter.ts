bfae2447fd89ba3c19471bd2815cb751e146cd36
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-09-17 21:59:51 +0200

    remove grid sample code

--- jarg-fe/src/game/game-starter.ts
+++ jarg-fe/src/game/game-starter.ts
@@ -1,9 +1,7 @@
 import { Application } from 'pixi.js';
-import GameService from './core/services/game-service';
 import Observer from './observers/observer';
 import GameCoordinator from './ui/game-coordinator';
 import UserService from './core/services/user-service';
-import GameSceneConstants from './ui/scene-coordinators/game-scene-constants';
 import ApplicationContext from './ui/application-context';
 import SceneManager from './ui/scene-coordinators/scene-manager';
 
@@ -13,9 +11,6 @@ export default async function setup(element: HTMLDivElement, window: Window) {
   const observer = new Observer();
   const ctx = new ApplicationContext(observer);
   ctx.setApp(app);
-
-  const gameService = new GameService(observer);
-  await gameService.init();
 
   const userService = new UserService(observer);
   await userService.init();
