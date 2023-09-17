66d5977cb9965e377547b72056d663fba39260e0
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-09-17 21:59:51 +0200

    remove grid sample code

--- jarg-fe/src/game/core/constants/grid-constants.ts
+++
@@ -1,9 +0,0 @@
-export class GridColors {
-  public static WHITE = 'white';
-  public static RED = 'red';
-  public static BLUE = 'blue';
-
-  public static values(): string[] {
-    return [GridColors.WHITE, GridColors.RED, GridColors.BLUE];
-  }
-}
