231d8203c62d495802fe07c7216cb4d231978580
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-09-18 20:18:01 +0200

    extract persona sheet; add buttons to sort list

--- jarg-fe/src/game/ui/scenes/personae-catalogue-scene.ts
+++ jarg-fe/src/game/ui/scenes/personae-catalogue-scene.ts
@@ -1,16 +1,17 @@
-import { Container, Text } from 'pixi.js';
+import { Container, DisplayObject, Text } from 'pixi.js';
 import Logger from '../../../logging/logger';
 import { AbstractGameScene } from './abstract-scene';
 import Fonts from '../styles/fonts';
-import { List, ScrollBox } from '@pixi/ui';
+import { Button, List, ScrollBox } from '@pixi/ui';
 import GameSceneConstants from '../scene-coordinators/game-scene-constants';
 import { Persona } from '../../core/models/persona';
 import ApplicationContext from '../application-context';
-import PixiNames from '../pixi-names';
 import ScreenData from '../devices/screen';
-import StringUtil from '../../util/string-util';
-import SimpleTable from '../components/simple-table';
-import ArrayUtil from '../../util/array-util';
+import PersonaSheetCompact from '../scene-elements/persona-sheet-compact';
+import PixiNames from '../pixi-names';
+
+type SortBy = 'level' | 'name';
+type SortOrder = 'asc' | 'desc';
 
 export default class PersonaeCatalogueScene extends AbstractGameScene {
   log = Logger.getInstance('PersonaeCatalogueScene');
@@ -18,9 +19,15 @@ export default class PersonaeCatalogueScene extends AbstractGameScene {
   personae: Array<Persona>;
   scrollBox?: ScrollBox;
 
+  sortBy?: SortBy;
+  sortOrder?: SortOrder;
+
   public constructor(ctx: ApplicationContext, personae: Array<Persona>) {
     super(ctx);
     this.personae = personae;
+    for (let i = 0; i < this.personae.length; i++) {
+      this.personae[i].level = Math.floor(Math.random() * 100);
+    }
   }
 
   name(): string {
@@ -38,6 +45,39 @@ export default class PersonaeCatalogueScene extends AbstractGameScene {
       marginTB = 5;
     }
 
+    const options = new List({ type: 'horizontal' });
+    options.addChild(
+      this.button('Sort by Name', () => {
+        if (this.sortBy === 'name') {
+          if (this.sortOrder === 'asc') {
+            this.sortOrder = 'desc';
+          } else {
+            this.sortOrder = 'asc';
+          }
+        } else {
+          this.sortBy = 'name';
+          this.sortOrder = 'asc';
+        }
+        this.updatePersonae(this.personae, this.sortBy, this.sortOrder);
+      })
+    );
+    options.addChild(
+      this.button('Sort by Lv.', () => {
+        if (this.sortBy === 'level') {
+          if (this.sortOrder === 'asc') {
+            this.sortOrder = 'desc';
+          } else {
+            this.sortOrder = 'asc';
+          }
+        } else {
+          this.sortBy = 'level';
+          this.sortOrder = 'asc';
+        }
+        this.updatePersonae(this.personae, this.sortBy, this.sortOrder);
+      })
+    );
+    this.getContainer().addChild(options);
+
     this.scrollBox = new ScrollBox({
       elementsMargin: 20,
       width: ScreenData.width() - 2 * marginLR,
@@ -48,79 +88,57 @@ export default class PersonaeCatalogueScene extends AbstractGameScene {
     this.scrollBox.y = marginTB;
 
     for (const persona of this.personae) {
-      const entry = await this.entry(persona);
-      this.scrollBox.addItem(entry);
+      const entry = new PersonaSheetCompact(this.scrollBox, this.ctx, persona, (p) => PixiNames.cataloguePersona(p));
+      const scrollBox = this.scrollBox;
+      entry.addToContainer = (content) => scrollBox.addItem(content);
+      await entry.start();
     }
+    this.addEntries(this.scrollBox, this.personae);
     this.getContainer().addChild(this.scrollBox);
   }
 
-  protected async entry(persona: Persona): Promise<Container> {
-    const entry = new Container();
-    entry.name = PixiNames.cataloguePersona(persona);
-    const col1 = new List({ type: 'vertical' });
-    const col2 = new List({ type: 'vertical' });
-    try {
-      const animation = await this.ctx.getAssetLoader().loadAnimatedSprite(persona.skin);
-      animation.width = 80;
-      animation.play();
-      col1.addChild(animation);
-    } catch (e) {
-      this.log.error(`Error while loading animation for ${persona.id}`, e);
+  protected async addEntries(scrollBox: ScrollBox, personae: Persona[]): Promise<void> {
+    for (const persona of personae) {
+      const entry = new PersonaSheetCompact(scrollBox, this.ctx, persona, (p) => PixiNames.cataloguePersona(p));
+      entry.addToContainer = (content) => scrollBox.addItem(content);
+      await entry.start();
     }
-
-    col1.addChild(this.text(persona.name + '/' + persona.race.name));
-    col1.addChild(this.text('Lv. ' + StringUtil.formatInt(persona.level)));
-    col1.addChild(
-      this.text(
-        ArrayUtil.join(
-          persona.classes.map((c) => `${c.def.name} (${c.level})`),
-          ' | '
-        )
-      )
-    );
-
-    const simpleTable = new SimpleTable();
-
-    simpleTable.addRow(
-      this.textSmall('HP'),
-      this.textSmall(StringUtil.formatInt(persona.hp.current) + '/' + StringUtil.formatInt(persona.hp.max)),
-      this.textSmall('MP'),
-      this.textSmall(StringUtil.formatInt(persona.mp.current) + '/' + StringUtil.formatInt(persona.mp.max))
-    );
-
-    simpleTable.addRow(
-      this.textSmall('ATK'),
-      this.textSmall(persona.baseStats.attack),
-      this.textSmall('DEF'),
-      this.textSmall(persona.baseStats.defence)
-    );
-
-    simpleTable.addRow(
-      this.textSmall('INT'),
-      this.textSmall(persona.baseStats.intelligence),
-      this.textSmall('RES'),
-      this.textSmall(persona.baseStats.resistance)
-    );
-
-    simpleTable.addOption('left');
-    simpleTable.addOption('right');
-    simpleTable.addOption('left');
-    simpleTable.addOption('right');
-    const stats = simpleTable.build();
-    //stats.y = col1.y + col1.height;
-    col2.addChild(stats);
-    col2.x = 300;
-    entry.addChild(col1);
-    entry.addChild(col2);
-    //entry.addChild(stats);
-    return entry;
   }
 
-  protected text(text: string | number): Text {
-    return new Text(StringUtil.format(text), Fonts.text());
+  protected async updatePersonae(personae: Persona[], sortBy: SortBy, order: SortOrder): Promise<void> {
+    if (!this.scrollBox) {
+      return;
+    }
+    const sorted = this.sortPersonae(personae, sortBy, order);
+    this.scrollBox.removeItems();
+    this.addEntries(this.scrollBox, sorted);
   }
 
-  protected textSmall(text: string | number): Text {
-    return new Text(StringUtil.format(text), Fonts.textSmall());
+  protected sortPersonae(personae: Persona[], sortBy: SortBy, order: SortOrder): Persona[] {
+    let sorted = personae;
+    if (sortBy === 'name') {
+      sorted = sorted.sort((p1, p2) => this.cmpStr(p1.name, p2.name));
+    }
+    if (sortBy === 'level') {
+      sorted = sorted.sort((p1, p2) => p1.level - p2.level);
+    }
+    if (order === 'desc') {
+      sorted = sorted.reverse();
+    }
+    return sorted;
+  }
+
+  protected cmpStr(str1: string, str2: string): number {
+    if (str1 === str2) {
+      return 0;
+    }
+    return str1 > str2 ? 1 : -1;
+  }
+
+  protected button(text: string, onClick: () => void): Container<DisplayObject> {
+    const element = new Text(text, Fonts.text());
+    const btn = new Button(element);
+    btn.onPress.connect(onClick);
+    return element;
   }
 }
