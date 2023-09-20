d7c3e4985e49fcb97cde719dce09c518f037cdd2
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-09-20 23:29:56 +0200

    simple mockup login page

---
+++ jarg-home/src/components/LoginComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>Sign in</p>
+    <div class="q-pa-md">
+      <div class="q-gutter-md row items-start">
+        <q-input
+          v-model="username"
+          class="col-12"
+          filled
+          type="text"
+          hint="Username"
+        >
+        </q-input>
+        <q-input
+          v-model="password"
+          class="col-12"
+          filled
+          :type="isPwd ? 'password' : 'text'"
+          hint="Password"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        <q-btn
+          class="col-12"
+          color="primary"
+          label="Primary"
+          @click="signIn()"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const username = ref('');
+const password = ref('');
+const isPwd = ref(true);
+
+const signIn = async (): Promise<void> => {
+  console.log('login');
+};
+</script>
---
+++ jarg-home/src/pages/IndexPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <login-component />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import LoginComponent from 'src/components/LoginComponent.vue';
+</script>
